2ed78cb7824b4dab63bd557cfaf60c9bf81f958e
CudaText-addons <cudatext@uvviewsoft.com>
2016-09-04 20:37:34 +0300

    non-Win error

--- __init__.py
+++ __init__.py
@@ -1,9 +1,14 @@
+import os
 from cudatext import *
 from .dlg import *
 from .reg_proc import *
 
 class Command:
     def dialog(self):
+        if os.name!='nt':
+        	msg_box('This plugin is for Windows only', MB_OK+MB_ICONERROR)
+        	return
+    
         old_menu = is_shell_extension_enabled()
         old_txt = is_file_assoc_enabled('txt')
         old_ini = is_file_assoc_enabled('ini')
@@ -21,4 +26,4 @@ class Command:
             if old_proj!=op_proj: apply_file_assoc('cuda-proj', op_proj)
             if old_sess!=op_sess: apply_file_assoc('cuda-session', op_sess)
         except:
-            msg_box('Cannot write registry key. Try to run program as Administrator.', MB_OK+MB_ICONWARNING)
+            msg_box('Cannot write registry key.\nTry to run program as Administrator.', MB_OK+MB_ICONWARNING)
